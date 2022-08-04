472a17281b6f633a8d50da0f2f382ff043089d2a
ch2423 <79548160+ch2423@users.noreply.github.com>
2022-08-04 08:00:41 -0500

    Update metric.example2.py

--- python/api-examples-source/metric.example2.py
+++ python/api-examples-source/metric.example2.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-col1, col2, col3 = st.columns(3)
-col1.metric("Temperature", "70 °F", "1.2 °F")
-col2.metric("Wind", "9 mph", "-8%")
-col3.metric("Humidity", "86%", "4%")
+row1, row2, row3 = st.rows(3)
+row1.metric("Temperature", "70 °F", "1.2 °F")
+row2.metric("Wind", "9 mph", "-8%")
+row3.metric("Humidity", "86%", "4%")
